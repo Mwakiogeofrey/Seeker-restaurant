87f43dee84266e1c1e7353d1f9a8f81732f622ed
Mwakiogeofrey <mwakiogeofrey579@gmail.com>
2024-06-26 15:18:30 +0300

    Modification

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 function App() {
   return (
     <div>
-    <Navbar expand="lg" className='fixed-top bg-body-tertiary shadow'>
+      <Navbar expand="lg" className='fixed-top bg-body-tertiary shadow'>
         <Container>
-          <Navbar.Brand >
+          <Navbar.Brand>
             <Link to="/" className='navbar-brand text-success fw-semibold'>
               Seeker Restaurant
             </Link>
@@ -20,28 +20,25 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto justify-content-end w-100">
-              <Nav.Link  href="/" className='active text-uppercase'>Home</Nav.Link>
-              <Nav.Link href ="Menu" className='active text-uppercase'>Menu </Nav.Link>
-              <Nav.Link href="About" className='active text-uppercase'>About</Nav.Link>
-              <Nav.Link href="Contact" className='active text-uppercase'>Contact</Nav.Link>
-              
-              
+              <Nav.Link as={Link} to="/" className='active text-uppercase'>Home</Nav.Link>
+              <Nav.Link as={Link} to="/Menu" className='active text-uppercase'>Menu</Nav.Link>
+              <Nav.Link as={Link} to="/About" className='active text-uppercase'>About</Nav.Link>
+              <Nav.Link as={Link} to="/Contact" className='active text-uppercase'>Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/Menu' element={<Menu/>}/>
-        <Route path='/About' element={<About/>}/>
-        <Route path='/Contact' element={<Contact/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/Menu' element={<Menu />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/Contact' element={<Contact />} />
       </Routes>
-  
+
       <footer className='bg-body-tertiary'>
-        <p className='p-3 m-0 text-center'>copyright @ made by Geofrey Killeta </p>
+        <p className='p-3 m-0 text-center'>copyright @ made by Geofrey Killeta</p>
       </footer>
-   
-      </div>
+    </div>
   );
 }
 
